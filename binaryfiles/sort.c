2aa584e196be8b813a7d1f6795a62f2aefae1db0
Artur Coelho <artur.temporal@hotmail.com>
2020-08-15 21:19:01 -0300

    qsort

--- binaryfiles/sort.c
+++ binaryfiles/sort.c
@@ -3,50 +3,8 @@
 #include <time.h>
 #define SIZE 10000000
 
-// void quick_sort(long int values[], int began, int end)
-// {
-// 	int i, j, pivo, aux;
-// 	i = began;
-// 	j = end-1;
-// 	pivo = values[(began + end) / 2];
-// 	while(i <= j)
-// 	{
-// 		while(values[i] < pivo && i < end) i++;
-// 		while(values[j] > pivo && j > began) j--;
-// 		if(i <= j)
-// 		{
-// 			aux = values[i];
-// 			values[i] = values[j];
-// 			values[j] = aux;
-// 			i++;
-// 			j--;
-// 		}
-// 	}
-// 	if(j > began)
-// 		quick_sort(values, began, j+1);
-// 	if(i < end)
-// 		quick_sort(values, i, end);
-// }
-
-void quick_sort(long int* arr, int beg, int end){
-    int i = beg, j = end - 1, mid = beg + end / 2;
-    int aux;
-
-    while(i <= j) {
-        while(arr[i] < mid && i < end) i++;
-        while(arr[j] > mid && j > beg) j--;
-        if (i <= j){
-            aux = arr[i];
-            arr[i] = arr[j];
-            arr[j] = aux;
-            i++;
-            j--;
-        }
-    }
-    if(j > beg)
-        quick_sort(arr, beg, j+1);
-    if(i < end)
-        quick_sort(arr, i, end);
+int cmpfunc (const void * a, const void * b) {
+   return ( *(long int*)a - *(long int*)b );
 }
 
 // escreve 10M longints em um arquivo
@@ -61,7 +19,7 @@ int main()
     long_int_f = fopen("./long_integers", "r");
     fread(long_int_a, sizeof(long int), SIZE, long_int_f);
     
-    quick_sort(long_int_a, 0, SIZE);
+    qsort(long_int_a, SIZE, sizeof(long int), cmpfunc);
 
 // escreve no arquivo em disco
     fwrite(long_int_a, sizeof(long int), SIZE, long_int_f);
